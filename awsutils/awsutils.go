dae07d72e2a66f6ebbcde4579ebb1c35a0dc67cc
Dimitri Stiliadis <dstiliadis@gmail.com>
2017-03-22 09:51:25 -0700

    AWS Utilities to retrieve instance ID and metadata

---
+++ awsutils/awsutils.go
@@ -0,0 +1,97 @@
+package awsutils
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+const (
+	metaDataPath    = "http://169.254.169.254/latest/meta-data/"
+	dynamicDataPath = "http://169.254.169.254/latest/dynamic/instance-identity"
+	pkcs7Name       = "pkcs7"
+	documentName    = "document"
+)
+
+// getValue retrieves the value from a metadata URI - just single value
+func getValue(uri string) (string, error) {
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+// getMetadataKeys retrieves all the metadaata keys that are not path levels
+// we ignore paths at this point
+func getMetadataKeys() ([]string, error) {
+	keyString, err := getValue(metaDataPath)
+	if err != nil {
+		return nil, err
+	}
+
+	keys := strings.Split(keyString, "\n")
+	valueKeys := []string{}
+
+	for _, key := range keys {
+		if strings.HasSuffix(key, "/") {
+			continue
+		}
+		valueKeys = append(valueKeys, key)
+	}
+
+	return valueKeys, nil
+}
+
+// getMetaData creates a Metadata map based on the available keys
+func getMetaData() (map[string]string, error) {
+
+	metadata := map[string]string{}
+
+	keys, err := getMetadataKeys()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, key := range keys {
+		value, err := getValue(metaDataPath + key)
+		if err != nil {
+			continue
+		}
+		metadata[key] = value
+	}
+
+	return metadata, nil
+}
+
+// getIdentity gets the PKCS7 identity document of an instance
+func getIdentity() (string, error) {
+
+	return getValue(dynamicDataPath + "/" + pkcs7Name)
+}
+
+// InstanceMetadata retrieves all the instance metadata in a map
+// It returns the PKCS7 ID, a map of the meta-data and error
+func InstanceMetadata() (string, map[string]string, error) {
+
+	metadata, err := getMetaData()
+	if err != nil {
+		return "", nil, err
+	}
+
+	id, err := getIdentity()
+	if err != nil {
+		return "", nil, err
+	}
+
+	return id, metadata, nil
+}

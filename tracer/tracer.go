f2fa7aafda1769070ec82f27f2f237ab7d55aecd
Alexandre Wilhelm <contact.dogild@gmail.com>
2017-04-18 10:56:11 -0700

    Fixed: ConfigureTracer now returns collector, sampler and tracer

--- tracer/tracer.go
+++ tracer/tracer.go
@@ -12,10 +12,10 @@ import (
 )
 
 // ConfigureTracer configure the tracer for opentracing with the given platform and cert pool
-func ConfigureTracer(pf *discovery.PlatformInfo, rootCAPool *x509.CertPool, serviceName string) error {
+func ConfigureTracer(pf *discovery.PlatformInfo, rootCAPool *x509.CertPool, serviceName string) (zipkin.Collector, zipkin.SpanRecorder, opentracing.Tracer, error) {
 
 	if pf.ZipkinURL == "" {
-		return nil
+		return nil, nil, nil, nil
 	}
 
 	httpClientOption := zipkin.HTTPClient(&http.Client{
@@ -29,16 +29,16 @@ func ConfigureTracer(pf *discovery.PlatformInfo, rootCAPool *x509.CertPool, serv
 	collector, e := zipkin.NewHTTPCollector(pf.ZipkinURL+"/api/v1/spans", httpClientOption)
 
 	if e != nil {
-		return e
+		return nil, nil, nil, e
 	}
 
 	recorder := zipkin.NewRecorder(collector, false, "0.0.0.0:0", serviceName)
 	tracer, e := zipkin.NewTracer(recorder, zipkin.ClientServerSameSpan(true), zipkin.TraceID128Bit(true))
 
 	if e != nil {
-		return e
+		return nil, nil, nil, e
 	}
 
 	opentracing.InitGlobalTracer(tracer)
-	return nil
+	return collector, recorder, tracer, nil
 }

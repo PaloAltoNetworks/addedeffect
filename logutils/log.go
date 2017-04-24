0a71bbc69e5c243bd4cb0b9d926c2a283589beec
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-04-24 15:14:46 -0700

    new: added logutils to configure zap logger

---
+++ logutils/log.go
@@ -0,0 +1,68 @@
+package logutils
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
+)
+
+// Configure configures the shared default logger.
+func Configure(level string, format string) {
+
+	var config zap.Config
+
+	switch format {
+	case "json":
+		config = zap.NewProductionConfig()
+	default:
+		config = zap.NewDevelopmentConfig()
+	}
+
+	// Set the logger
+	switch level {
+	case "trace", "debug":
+		config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	case "info":
+		config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	case "warn":
+		config.Level = zap.NewAtomicLevelAt(zap.WarnLevel)
+	case "error":
+		config.Level = zap.NewAtomicLevelAt(zap.ErrorLevel)
+	case "fatal":
+		config.Level = zap.NewAtomicLevelAt(zap.FatalLevel)
+	default:
+		config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	}
+
+	logger, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	zap.ReplaceGlobals(logger)
+
+	go func(config zap.Config) {
+
+		defaultLevel := config.Level
+		var elevated bool
+
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGUSR1)
+		for s := range c {
+			if s == syscall.SIGINT {
+				return
+			}
+			elevated = !elevated
+
+			if elevated {
+				config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+				zap.L().Info("Log level elevated to debug")
+			} else {
+				zap.L().Info("Log level restored to original configuration", zap.String("level", level))
+				config.Level = defaultLevel
+			}
+		}
+	}(config)
+}

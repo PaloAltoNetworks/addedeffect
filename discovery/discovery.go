4bf8b4824c8711890f643a6ebb6c59c3ac1737ca
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-07-13 20:37:54 -0700

    typo

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -140,7 +140,7 @@ func (p *PlatformInfo) Fields() []zapcore.Field {
 		zap.Strings("mongo", p.MongoServices),
 		zap.Strings("cassandra", p.CassandraServices),
 		zap.String("influxdb", p.InfluxDBURL),
-		zap.String("geoio", p.GeoIPURL),
+		zap.String("geoip", p.GeoIPURL),
 		zap.Strings("nats", p.PubSubServices),
 		zap.String("system-version", p.SystemVersion),
 	}

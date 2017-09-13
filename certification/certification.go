029d91d172923f06d88c1ef9280e4e107872afcd
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-09-12 17:39:30 -0700

    cleanup

--- certification/certification.go
+++ certification/certification.go
@@ -154,6 +154,46 @@ func GenerateSimpleCSR(orgs []string, units []string, commonName string, emails 
 	return pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE REQUEST", Bytes: csrDerBytes}), nil
 }
 
+// SplitChain splits the given certificate data into the actual *x509.Certificate and a list of
+// CA chain in a []*x509.Certificate
+func SplitChain(certData []byte) (cert *x509.Certificate, caChain []*x509.Certificate, err error) {
+
+	block, rest := pem.Decode(certData)
+
+	for ; block != nil; block, rest = pem.Decode(rest) {
+
+		if block.Type != "CERTIFICATE" {
+			continue
+		}
+
+		crt, err := x509.ParseCertificate(block.Bytes)
+
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if !crt.IsCA {
+			cert = crt
+			continue
+		}
+
+		if len(rest) != 0 {
+			caChain = append(caChain, crt)
+		}
+	}
+
+	return
+}
+
+// SplitChainPEM splits the given cert PEM []byte as the actual certificate
+// and []byte as the rest of the chain.
+func SplitChainPEM(certData []byte) ([]byte, []byte) {
+
+	block, rest := pem.Decode(certData)
+
+	return pem.EncodeToMemory(block), rest
+}
+
 // IssueCert asks and returns an new certificate using the given barret Manipulator and given CSR.
 // You can generate easily a CSR using GenerateSimpleCSR.
 func IssueCert(m manipulate.Manipulator, csrPEM []byte, expiration time.Time, usage KeyUsage) (cert []byte, serialNumber string, exp time.Time, err error) {

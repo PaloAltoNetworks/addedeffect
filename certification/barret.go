e0f77427a398a085a223592ab7cf56023216c7fb
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-10-12 19:39:32 -0700

    fixed: use new maniphttp api

--- certification/barret.go
+++ certification/barret.go
@@ -290,7 +290,7 @@ func CreateServiceCertificates(
 		zap.L().Fatal("Unable to decode issuing certificate key pair", zap.Error(err))
 	}
 
-	issuingManipulator := maniphttp.NewHTTPManipulatorWithMTLS(pf.BarretURL, rootCAPool, nil, []tls.Certificate{issuingCertKeyPair}, false)
+	issuingManipulator := maniphttp.NewHTTPManipulatorWithMTLS(pf.BarretURL, rootCAPool, []tls.Certificate{issuingCertKeyPair}, false)
 
 	if getClientCert {
 		clientCert, err = IssueServiceClientCertificate(issuingManipulator, serviceName, 8760*time.Hour)

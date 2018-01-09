b8da7daa37b3c9a9a19be86b41acbf8de3ade1bb
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-01-08 16:16:55 -0800

    new: added package updatesync

---
+++ updatesync/updatesync.go
@@ -0,0 +1,74 @@
+package updatesync
+
+import (
+	"github.com/aporeto-inc/elemental"
+	"github.com/aporeto-inc/manipulate"
+)
+
+// UpdateSync is an advanced function that allows to retry an failed update operation due to an
+// updateTime mismatch (basically an update occurred between the time you got a copy of an object
+// and the time you try to update it.
+//
+// It gets a manipulator and context to perform the updatea dnd also a function that returns an identifiable.
+// This function must be used to apply the desired update. IT MUST NOT ALLOCATE A NEW OBJECT.
+// For perf reasons, UpdateSync does not enforce that the return of your function must be a pointer.
+//
+// UpdateSync gets the Identifiable you want to update using your function and tries to update
+// it. If it fails because of a desync, it will retrieve the latest copy of your object (modifying it)
+// then will apply your function again (so you can reaply your desired changes) and try again until
+// it suceeds or until the max number of tries is reached.
+//
+// Example:
+//
+//      // If we have this object unit in memory
+//      o := &Object{
+//          Name: "Hello World",
+//          Description: "This is the original description"
+//      }
+//
+//      // Then if we want to change the description, even if an update happened, we do:
+//      err := UpdateSync(m, nil, 5, func() elemental.Identifiable {
+//          o.Description = "This is the description I want!" // USE OUTER CONTEXT TO GET O
+//          return o
+//      }
+//
+// Please keep in mind you have to be very careful with this function. You may still put the target object
+// in a very weird state, if you override attributes that can be managed by a different source of truth.
+func UpdateSync(
+	m manipulate.Manipulator,
+	mctx *manipulate.Context,
+	maxTry int,
+	updateFunc func() elemental.Identifiable,
+) error {
+
+	var try int
+
+	for {
+
+		try++
+
+		obj := updateFunc()
+
+		err := manipulate.Retry(func() error { return m.Update(mctx, obj) }, nil, 10)
+		if err == nil {
+			return nil
+		}
+
+		// If we reach the maximum number of try, we return the error.
+		if try >= maxTry {
+			return err
+		}
+
+		// If the error is not a validation error for read only update time, we return the error.
+		if !elemental.IsValidationError(err, "Read Only Error", "updateTime") {
+			return err
+		}
+
+		// Otherwise we retrieve the latest copy of the object.
+		if err = m.Retrieve(mctx, obj); err != nil {
+			return err
+		}
+
+		// Then the loop will run again trying to update the object by applying the updateFunc on it.
+	}
+}
---
+++ updatesync/updatesync_test.go
@@ -0,0 +1,156 @@
+package updatesync
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/aporeto-inc/elemental"
+	"github.com/aporeto-inc/gaia/squallmodels/v1/golang"
+	"github.com/aporeto-inc/manipulate"
+	"github.com/aporeto-inc/manipulate/maniptest"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAPI_UpdateSync(t *testing.T) {
+
+	Convey("Given I have a manipulator an object and an update func", t, func() {
+
+		var synced int
+		m := maniptest.NewTestManipulator()
+		o := squallmodels.NewProcessingUnit()
+		o.Name = "name-original"
+		o.Description = "desc-original"
+		uf := func() elemental.Identifiable {
+			synced++
+			o.Description = "synced!"
+			return o
+		}
+
+		Convey("When I update my object there is no sync needed", func() {
+
+			m.MockUpdate(t, func(ctx *manipulate.Context, objects ...elemental.Identifiable) error {
+				return nil
+			})
+
+			err := UpdateSync(m, nil, 10, uf)
+
+			Convey("Then err should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Then obj should have been updated", func() {
+				So(o.Description, ShouldEqual, "synced!")
+				So(o.Name, ShouldEqual, "name-original")
+			})
+
+			Convey("Then the updateFunc should have been called once", func() {
+				So(synced, ShouldEqual, 1)
+			})
+		})
+
+		Convey("When I update my object there is a sync needed", func() {
+
+			m.MockUpdate(t, func(ctx *manipulate.Context, objects ...elemental.Identifiable) error {
+				objects[0].(*squallmodels.ProcessingUnit).Name = fmt.Sprintf("sync%d", synced)
+
+				if synced <= 3 {
+					e := elemental.NewError("Read Only Error", "bloob", "subject", http.StatusUnprocessableEntity)
+					e.Data = map[string]interface{}{"attribute": "updateTime"}
+					return e
+				}
+
+				return nil
+			})
+
+			err := UpdateSync(m, nil, 10, uf)
+
+			Convey("Then err should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Then obj should have been updated", func() {
+				So(o.Name, ShouldEqual, "sync4")
+				So(o.Description, ShouldEqual, "synced!")
+			})
+
+			Convey("Then the updateFunc should have been called four times", func() {
+				So(synced, ShouldEqual, 4)
+			})
+		})
+
+		Convey("When I update my object but there is an error right away", func() {
+
+			m.MockUpdate(t, func(ctx *manipulate.Context, objects ...elemental.Identifiable) error {
+				return elemental.NewError("Not Read Only Error", "bloob", "subject", http.StatusInternalServerError)
+			})
+
+			err := UpdateSync(m, nil, 10, uf)
+
+			Convey("Then err should not be nil", func() {
+				So(err, ShouldNotBeNil)
+			})
+
+			// TODO: that would be ideal, but this involves deep copy,
+			// and I'm not confident about it.
+			// Convey("Then obj should not have been updated", func() {
+			// 	So(o.Name, ShouldEqual, "name-original")
+			// 	So(o.Description, ShouldEqual, "desc-original")
+			// })
+
+			Convey("Then the updateFunc should have been called once", func() {
+				So(synced, ShouldEqual, 1)
+			})
+		})
+
+		Convey("When I update my object there is a sync needed but if fails more than the number of tries", func() {
+
+			m.MockUpdate(t, func(ctx *manipulate.Context, objects ...elemental.Identifiable) error {
+				e := elemental.NewError("Read Only Error", "bloob", "subject", http.StatusUnprocessableEntity)
+				e.Data = map[string]interface{}{"attribute": "updateTime"}
+				return e
+			})
+
+			err := UpdateSync(m, nil, 2, uf)
+
+			Convey("Then err should not be nil", func() {
+				So(err, ShouldNotBeNil)
+			})
+
+			Convey("Then the updateFunc should have been called four times", func() {
+				So(synced, ShouldEqual, 2)
+			})
+		})
+
+		Convey("When I update my object there is a sync needed but the retrieve fails", func() {
+
+			m.MockUpdate(t, func(ctx *manipulate.Context, objects ...elemental.Identifiable) error {
+				objects[0].(*squallmodels.ProcessingUnit).Name = fmt.Sprintf("sync%d", synced)
+
+				if synced <= 3 {
+					e := elemental.NewError("Read Only Error", "bloob", "subject", http.StatusUnprocessableEntity)
+					e.Data = map[string]interface{}{"attribute": "updateTime"}
+					return e
+				}
+
+				return nil
+			})
+
+			m.MockRetrieve(t, func(ctx *manipulate.Context, objects ...elemental.Identifiable) error {
+				return fmt.Errorf("boom")
+			})
+
+			err := UpdateSync(m, nil, 10, uf)
+
+			Convey("Then err should not be nil", func() {
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "boom")
+			})
+
+			Convey("Then the updateFunc should have been called four times", func() {
+				So(synced, ShouldEqual, 1)
+			})
+		})
+	})
+}

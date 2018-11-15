125e456fc25a8e60eb863bff61b6eba70e9571b1
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-11-15 13:15:55 -0800

    new: added appcreds package to help create and renew appcreds

---
+++ appcreds/appcreds.go
@@ -0,0 +1,79 @@
+package appcreds
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/pem"
+
+	"go.aporeto.io/gaia"
+	"go.aporeto.io/manipulate"
+	"go.aporeto.io/tg/tglib"
+)
+
+// New creates a new *gaia.AppCredential.
+func New(ctx context.Context, m manipulate.Manipulator, namespace string, name string, roles []string) (*gaia.AppCredential, error) {
+
+	creds := gaia.NewAppCredential()
+	creds.Name = name
+	creds.Roles = roles
+
+	if err := m.Create(
+		manipulate.NewContext(
+			ctx,
+			manipulate.ContextOptionNamespace(namespace),
+		),
+		creds,
+	); err != nil {
+		return nil, err
+	}
+
+	return Renew(ctx, m, creds)
+}
+
+// Renew renews the given appcred.
+func Renew(ctx context.Context, m manipulate.Manipulator, creds *gaia.AppCredential) (*gaia.AppCredential, error) {
+
+	// Then we generate a private key and a CSR from the appcred info.
+	csr, pk, err := makeCSR(creds.Name, creds.ID, creds.Namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	// And we update the appcred with the csr
+	creds.CSR = string(csr)
+
+	if err = m.Update(
+		manipulate.NewContext(
+			ctx,
+			manipulate.ContextOptionNamespace(creds.Namespace),
+		),
+		creds,
+	); err != nil {
+		return nil, err
+	}
+
+	// And we write the private key in the appcred.
+	creds.Credentials.CertificateKey = base64.StdEncoding.EncodeToString(pk)
+
+	return creds, nil
+}
+
+func makeCSR(name string, id string, namespace string) (csr []byte, key []byte, err error) {
+
+	pk, err := tglib.ECPrivateKeyGenerator()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	csr, err = tglib.GenerateSimpleCSR([]string{namespace}, nil, "app:credential:"+id+":"+name, nil, pk)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	keyBlock, err := tglib.KeyToPEM(pk)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return csr, pem.EncodeToMemory(keyBlock), nil
+}
---
+++ appcreds/appcreds_test.go
@@ -0,0 +1,147 @@
+package appcreds
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"go.aporeto.io/tg/tglib"
+
+	"go.aporeto.io/elemental"
+	"go.aporeto.io/gaia"
+	"go.aporeto.io/manipulate"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"go.aporeto.io/manipulate/maniptest"
+)
+
+func TestApCred_New(t *testing.T) {
+
+	Convey("Given I have a manipulator", t, func() {
+
+		m := maniptest.NewTestManipulator()
+
+		m.MockCreate(t, func(ctx manipulate.Context, objects ...elemental.Identifiable) error {
+
+			if ctx.Namespace() != "/ns" {
+				panic("expected ns to be /ns")
+			}
+
+			ac := objects[0].(*gaia.AppCredential)
+			ac.ID = "ID"
+			ac.Namespace = "/ns"
+
+			return nil
+		})
+
+		var expectedCSR string
+		m.MockUpdate(t, func(ctx manipulate.Context, objects ...elemental.Identifiable) error {
+
+			if ctx.Namespace() != "/ns" {
+				panic("expected ns to be /ns")
+			}
+
+			ac := objects[0].(*gaia.AppCredential)
+			ac.Credentials = gaia.NewCredential()
+			ac.Credentials.APIURL = "https://labas"
+			ac.Credentials.Name = ac.Name
+			ac.Credentials.Namespace = ac.Namespace
+			// ac.Credentials.
+
+			expectedCSR = ac.CSR
+
+			return nil
+		})
+
+		Convey("When I call New", func() {
+
+			c, err := New(context.Background(), m, "/ns", "name", []string{"@auth:role=role1"})
+
+			Convey("Then err should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Then the cred should be correct", func() {
+				So(c.Name, ShouldEqual, "name")
+				So(c.ID, ShouldEqual, "ID")
+				So(c.Namespace, ShouldEqual, "/ns")
+				So(c.Credentials.CertificateKey, ShouldNotBeEmpty)
+			})
+
+			Convey("When I verify the csr", func() {
+
+				csrs, err := tglib.LoadCSRs([]byte(expectedCSR))
+
+				Convey("Then err should be nil", func() {
+					So(err, ShouldBeNil)
+				})
+
+				Convey("Then csr should be correct", func() {
+					So(len(csrs), ShouldEqual, 1)
+					So(csrs[0].Subject.CommonName, ShouldEqual, "app:credential:ID:name")
+					So(len(csrs[0].Subject.Organization), ShouldEqual, 1)
+					So(csrs[0].Subject.Organization[0], ShouldEqual, "/ns")
+				})
+			})
+		})
+	})
+
+	Convey("Given I have a manipulator that fails at creation", t, func() {
+
+		m := maniptest.NewTestManipulator()
+
+		m.MockCreate(t, func(ctx manipulate.Context, objects ...elemental.Identifiable) error {
+			return fmt.Errorf("boom")
+		})
+
+		Convey("When I call New", func() {
+
+			c, err := New(context.Background(), m, "/ns", "name", []string{"@auth:role=role1"})
+
+			Convey("Then err should not be nil", func() {
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "boom")
+			})
+
+			Convey("Then the cred should be nilt", func() {
+				So(c, ShouldBeNil)
+			})
+		})
+	})
+
+	Convey("Given I have a manipulator that fails at update", t, func() {
+
+		m := maniptest.NewTestManipulator()
+
+		m.MockCreate(t, func(ctx manipulate.Context, objects ...elemental.Identifiable) error {
+
+			if ctx.Namespace() != "/ns" {
+				panic("expected ns to be /ns")
+			}
+
+			ac := objects[0].(*gaia.AppCredential)
+			ac.ID = "ID"
+			ac.Namespace = "/ns"
+
+			return nil
+		})
+
+		m.MockUpdate(t, func(ctx manipulate.Context, objects ...elemental.Identifiable) error {
+			return fmt.Errorf("paf")
+		})
+
+		Convey("When I call New", func() {
+
+			c, err := New(context.Background(), m, "/ns", "name", []string{"@auth:role=role1"})
+
+			Convey("Then err should not be nil", func() {
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "paf")
+			})
+
+			Convey("Then the cred should be nilt", func() {
+				So(c, ShouldBeNil)
+			})
+		})
+	})
+}

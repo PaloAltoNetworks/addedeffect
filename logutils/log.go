3c8fab8e0a8f34578fe42eda3dc81c95889f98ee
Satyam Sinha <satyamsi@users.noreply.github.com>
2019-05-07 02:08:35 -0700

    Refactor (#57)

--- logutils/log.go
+++ logutils/log.go
@@ -12,6 +12,7 @@
 package logutils
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"time"
@@ -36,7 +37,7 @@ func Configure(level string, format string) zap.Config {
 // ConfigureWithName configures the shared default logger.
 func ConfigureWithName(serviceName string, level string, format string) zap.Config {
 
-	logger, config := NewLogger(serviceName, level, format, "", false, false)
+	logger, config := newLogger(serviceName, level, format, "", false, false)
 
 	zap.ReplaceGlobals(logger)
 
@@ -48,7 +49,7 @@ func ConfigureWithName(serviceName string, level string, format string) zap.Conf
 // ConfigureWithOptions configures the shared default logger with options such as file and timestamp formats.
 func ConfigureWithOptions(level string, format string, file string, fileOnly bool, prettyTimestamp bool) zap.Config {
 
-	logger, config := NewLogger("", level, format, file, fileOnly, prettyTimestamp)
+	logger, config := newLogger("", level, format, file, fileOnly, prettyTimestamp)
 
 	zap.ReplaceGlobals(logger)
 
@@ -57,61 +58,10 @@ func ConfigureWithOptions(level string, format string, file string, fileOnly boo
 	return config
 }
 
-// NewLogger returns a new configured zap.Logger
-func NewLogger(serviceName string, level string, format string, file string, fileOnly bool, prettyTimestamp bool) (*zap.Logger, zap.Config) {
+// newLogger returns a new configured zap.Logger
+func newLogger(serviceName string, level string, format string, file string, fileOnly bool, prettyTimestamp bool) (*zap.Logger, zap.Config) {
 
-	var config zap.Config
-
-	var initialFields map[string]interface{}
-	if serviceName != "" {
-		initialFields = map[string]interface{}{
-			"srv": serviceName,
-		}
-	}
-
-	switch format {
-	case "json":
-		config = zap.NewProductionConfig()
-		config.DisableStacktrace = true
-		config.EncoderConfig.CallerKey = "c"
-		config.EncoderConfig.LevelKey = "l"
-		config.EncoderConfig.MessageKey = "m"
-		config.EncoderConfig.NameKey = "n"
-		config.EncoderConfig.TimeKey = "t"
-
-		config.InitialFields = initialFields
-
-	case "stackdriver":
-		config = zap.NewProductionConfig()
-		config.EncoderConfig.LevelKey = "severity"
-		config.EncoderConfig.EncodeLevel = func(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
-			switch l {
-			case zapcore.DebugLevel:
-				enc.AppendString("DEBUG")
-			case zapcore.InfoLevel:
-				enc.AppendString("INFO")
-			case zapcore.WarnLevel:
-				enc.AppendString("WARNING")
-			case zapcore.ErrorLevel:
-				enc.AppendString("ERROR")
-			case zapcore.DPanicLevel:
-				enc.AppendString("CRITICAL")
-			case zapcore.PanicLevel:
-				enc.AppendString("ALERT")
-			case zapcore.FatalLevel:
-				enc.AppendString("EMERGENCY")
-			}
-		}
-
-		config.InitialFields = initialFields
-
-	default:
-		config = zap.NewDevelopmentConfig()
-		config.DisableStacktrace = true
-		config.DisableCaller = true
-		config.EncoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {}
-		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	}
+	config := getConfig(serviceName, format)
 
 	// Handle log file output
 	w, err := handleOutputFile(&config, file, fileOnly)
@@ -127,23 +77,9 @@ func NewLogger(serviceName string, level string, format string, file string, fil
 	}
 
 	// Set the logger
-	switch level {
-	case "trace", "debug":
-		config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
-	case "info":
-		config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
-	case "warn":
-		config.Level = zap.NewAtomicLevelAt(zap.WarnLevel)
-	case "error":
-		config.Level = zap.NewAtomicLevelAt(zap.ErrorLevel)
-	case "fatal":
-		config.Level = zap.NewAtomicLevelAt(zap.FatalLevel)
-	default:
-		config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
-	}
+	config.Level = levelToZapLevel(level)
 
-	logger := initLogger(w, config)
-
+	logger, err := initLogger(w, config)
 	if err != nil {
 		panic(err)
 	}
@@ -151,37 +87,134 @@ func NewLogger(serviceName string, level string, format string, file string, fil
 	return logger, config
 }
 
-// initLogger constructs the logger from the options
-func initLogger(w zapcore.WriteSyncer, conf zap.Config) *zap.Logger {
-	var enc zapcore.Encoder
-	var core, coreFile, coreConsole zapcore.Core
+// getConfig provides a zap configuration
+func getConfig(serviceName, format string) zap.Config {
 
-	switch conf.Encoding {
+	var initialFields map[string]interface{}
+	if serviceName != "" {
+		initialFields = map[string]interface{}{
+			"srv": serviceName,
+		}
+	}
+
+	switch format {
 	case "json":
-		enc = zapcore.NewJSONEncoder(conf.EncoderConfig)
-	case "console":
-		enc = zapcore.NewConsoleEncoder(conf.EncoderConfig)
+		return getJSONConfig(initialFields)
+
+	case "stackdriver":
+		return getJSONConfig(initialFields)
+
 	default:
-		panic("unknown encoding")
+		return getDefaultConfig()
+	}
+}
+
+// getJSONConfig provides a JSON zap configuration
+func getJSONConfig(initialFields map[string]interface{}) zap.Config {
+
+	config := zap.NewProductionConfig()
+	config.DisableStacktrace = true
+	config.EncoderConfig.CallerKey = "c"
+	config.EncoderConfig.LevelKey = "l"
+	config.EncoderConfig.MessageKey = "m"
+	config.EncoderConfig.NameKey = "n"
+	config.EncoderConfig.TimeKey = "t"
+
+	config.InitialFields = initialFields
+
+	return config
+}
+
+// getStackdriverConfig provides a stackdriver zap configuration
+func getStackdriverConfig(initialFields map[string]interface{}) zap.Config {
+
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.LevelKey = "severity"
+	config.EncoderConfig.EncodeLevel = func(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+		switch l {
+		case zapcore.DebugLevel:
+			enc.AppendString("DEBUG")
+		case zapcore.InfoLevel:
+			enc.AppendString("INFO")
+		case zapcore.WarnLevel:
+			enc.AppendString("WARNING")
+		case zapcore.ErrorLevel:
+			enc.AppendString("ERROR")
+		case zapcore.DPanicLevel:
+			enc.AppendString("CRITICAL")
+		case zapcore.PanicLevel:
+			enc.AppendString("ALERT")
+		case zapcore.FatalLevel:
+			enc.AppendString("EMERGENCY")
+		}
 	}
 
-	console := zapcore.Lock(os.Stderr)
-	coreConsole = zapcore.NewCore(enc, console, conf.Level)
+	config.InitialFields = initialFields
 
-	if w == nil {
+	return config
+}
+
+func getDefaultConfig() zap.Config {
+
+	config := zap.NewDevelopmentConfig()
+	config.DisableStacktrace = true
+	config.DisableCaller = true
+	config.EncoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {}
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+
+	return config
+}
+
+// levelToZapLevel provides a zapLevel given a level configuration
+func levelToZapLevel(level string) zap.AtomicLevel {
+
+	switch level {
+	case "trace", "debug":
+		return zap.NewAtomicLevelAt(zap.DebugLevel)
+	case "info":
+		return zap.NewAtomicLevelAt(zap.InfoLevel)
+	case "warn":
+		return zap.NewAtomicLevelAt(zap.WarnLevel)
+	case "error":
+		return zap.NewAtomicLevelAt(zap.ErrorLevel)
+	case "fatal":
+		return zap.NewAtomicLevelAt(zap.FatalLevel)
+	default:
+		return zap.NewAtomicLevelAt(zap.InfoLevel)
+	}
+}
+
+// getEncoder provides an encoder based on encoding configuration
+func getEncoder(c zap.Config) (zapcore.Encoder, error) {
+
+	switch c.Encoding {
+	case "json":
+		return zapcore.NewJSONEncoder(c.EncoderConfig), nil
+	case "console":
+		return zapcore.NewConsoleEncoder(c.EncoderConfig), nil
+	default:
+		return nil, errors.New("unknown encoding")
+	}
+}
+
+// initLogger constructs the logger from the options
+func initLogger(w zapcore.WriteSyncer, conf zap.Config) (*zap.Logger, error) {
+
+	enc, err := getEncoder(conf)
+	if err != nil {
+		return nil, err
+	}
+
+	core := zapcore.NewCore(enc, zapcore.Lock(os.Stderr), conf.Level)
+
+	if w != nil {
 		core = zapcore.NewTee(
-			coreConsole,
-		)
-	} else {
-		coreFile = zapcore.NewCore(enc, w, conf.Level)
-		core = zapcore.NewTee(
-			coreFile,
-			coreConsole,
+			zapcore.NewCore(enc, w, conf.Level),
+			core,
 		)
 	}
 
-	logger := zap.New(core)
-	return logger
+	return zap.New(core), nil
 }
 
 // handleOutputFile handles options in log configs to redirect to file
